9c270596845957a24f8677c2063d3e53581f01b5
Victor Baleeiro <victor.baleeiro@going2.com.br>
2019-01-08 19:45:47 -0200

    finalizado o exemplo de tela de chat messages (front end)

--- ios-examples/ios-examples/group-message/ChatMessageCell.swift
+++ ios-examples/ios-examples/group-message/ChatMessageCell.swift
@@ -11,24 +11,68 @@ import UIKit
 class ChatMessageCell: UITableViewCell {
     
     let messageLabel = UILabel()
+    let bubbleBackgroundView = UIView()
+    
+    var chatMessage: ChatMessage! {
+        didSet {
+            bubbleBackgroundView.backgroundColor = chatMessage.isIncoming ? .white : .darkGray
+            messageLabel.textColor = chatMessage.isIncoming ? .black : .white
+            
+            messageLabel.text = chatMessage.text
+            
+            if chatMessage.isIncoming {
+                leadingConstraint.isActive = true
+                trailingConstraint.isActive = false
+            } else {
+                leadingConstraint.isActive = false
+                trailingConstraint.isActive = true
+            }
+        }
+    }
+    
+//    var isIncoming: Bool! {
+//        didSet {
+//            bubbleBackgroundView.backgroundColor = isIncoming ? .white : .darkGray
+//            messageLabel.textColor = isIncoming ? .black : .white
+//        }
+//    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        
+        bubbleBackgroundView.backgroundColor = .yellow
+        bubbleBackgroundView.layer.cornerRadius = 12
+        bubbleBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bubbleBackgroundView)
         
         addSubview(messageLabel)
-        messageLabel.backgroundColor = .green
-        messageLabel.text = "some message"
+        //messageLabel.backgroundColor = .green
+        messageLabel.text = "if we could party all night and sleep all day and throw all of my problems away my life would be easy my life would be easy"
+        messageLabel.numberOfLines = 0
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let contraints = [ messageLabel.topAnchor.constraint(equalTo: topAnchor),
-        messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-        messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-        messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor)]
-        
+        let contraints = [ messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+                           messageLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
+                           messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+                           
+                           bubbleBackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -16),
+                           bubbleBackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -16),
+                           bubbleBackgroundView.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 16),
+                           bubbleBackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16)
+        ]
         NSLayoutConstraint.activate(contraints)
-        //messageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        leadingConstraint = messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32)
+        leadingConstraint.isActive = false
+        
+        trailingConstraint = messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
+        trailingConstraint.isActive = true
     }
     
+    var leadingConstraint: NSLayoutConstraint!
+    var trailingConstraint: NSLayoutConstraint!
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- ios-examples/ios-examples/group-message/ViewController.swift
+++ ios-examples/ios-examples/group-message/ViewController.swift
@@ -8,27 +8,148 @@
 
 import UIKit
 
+struct ChatMessage {
+    let text: String
+    let isIncoming: Bool
+    let date: Date
+}
+
+extension Date {
+    static func dateFromCustomString(customString: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.date(from: customString) ?? Date()
+    }
+}
+
 class ViewController: UITableViewController {
 
     fileprivate let cellId = "id"
+//    fileprivate let chatMessages = [
+//        [
+//            ChatMessage(text: "My first short message", isIncoming: true, date: Date.dateFromCustomString(customString: "03/01/2019")),
+//            ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "03/01/2019")),
+//        ],
+//        [
+//            ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, Yo, how are you", isIncoming: false, date: Date.dateFromCustomString(customString: "05/01/2019")),
+//            ChatMessage(text: "Yo, howwwww", isIncoming: false, date: Date.dateFromCustomString(customString: "05/01/2019")),
+//            ChatMessage(text: "buble test sopokaoskokaoskoakoskoaok", isIncoming: true, date: Date.dateFromCustomString(customString: "05/01/2019")),
+//        ],
+//        [
+//            ChatMessage(text: "Third section", isIncoming: true, date: Date.dateFromCustomString(customString: "08/01/2019")),
+//        ]
+//    ]
+    fileprivate let messagesFromServer = [
+        ChatMessage(text: "My first short message", isIncoming: true, date: Date.dateFromCustomString(customString: "03/01/2019")),
+        ChatMessage(text: "I'm going to message another long message that will word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "03/01/2019")),
+        ChatMessage(text: "I'm going to message another long message that will word wrap, I'm going to message another long message that will word wrap, Yo, how are you", isIncoming: false, date: Date.dateFromCustomString(customString: "05/01/2019")),
+        ChatMessage(text: "Yo, howwwww", isIncoming: false, date: Date.dateFromCustomString(customString: "05/01/2019")),
+        ChatMessage(text: "buble test sopokaoskokaoskoakoskoaok", isIncoming: true, date: Date.dateFromCustomString(customString: "05/01/2019")),
+        ChatMessage(text: "Third section", isIncoming: true, date: Date.dateFromCustomString(customString: "08/01/2019")),
+    ]
+    fileprivate var chatMessages = [[ChatMessage]]()
+    fileprivate func attemptToAssembleGroupedMessages() {
+        print("attemptToAssembleGroupedMessages")
+        
+        let groupedMessages = Dictionary(grouping: messagesFromServer) { (element) -> Date in
+            return element.date
+        }
+        
+        let sortedKeys = groupedMessages.keys.sorted()
+        sortedKeys.forEach { (key) in
+            let values = groupedMessages[key]
+            chatMessages.append(values ?? [])
+        }
+        
+//        groupedMessages.keys.forEach { (key) in
+//            let values = groupedMessages[key]
+//            chatMessages.append(values ?? [])
+//        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        attemptToAssembleGroupedMessages()
+        
         title = "Messages"
         navigationController?.navigationBar.prefersLargeTitles = true
         tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellId)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return chatMessages.count
+    }
+    
+    class DateHeaderLabel: UILabel {
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            backgroundColor = .black
+            textColor = .white
+            textAlignment = .center
+            translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            font = .boldSystemFont(ofSize: 14)
+            let originalContentSize = super.intrinsicContentSize
+            let height = originalContentSize.height + 16
+            layer.cornerRadius = height / 2
+            layer.masksToBounds = true
+            return CGSize(width: originalContentSize.width + 16, height: height)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if let firstMessageInSection = chatMessages[section].first {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+            let dateString = dateFormatter.string(from: firstMessageInSection.date)
+        
+            let label = DateHeaderLabel()
+            label.text = dateString
+            let containerView = UIView()
+            containerView.addSubview(label)
+            label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+            return containerView
+        }
+        
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        if let firstMessageInSection = chatMessages[section].first {
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.dateFormat = "dd/MM/yyyy"
+//            let dateString = dateFormatter.string(from: firstMessageInSection.date)
+//            return dateString
+//        }
+//        return "Section \(Date())"
+//    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return chatMessages[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        //cell.textLabel?.text = "a long string to test message...a long string to test message...a long string to test messagea long string to test message...a long string to test message...a long string to test message...a long string to test message...a long string to test message...a long string to test message...a long string to test message...a long string to test message..."
-        cell.textLabel?.numberOfLines = 0
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatMessageCell
+        let chatMessage = chatMessages[indexPath.section][indexPath.row]
+        cell.chatMessage = chatMessage
         return cell
     }
 }
